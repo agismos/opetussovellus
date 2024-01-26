6da17640e8b41c898eefcbe03377a491da73760c
Ismo <ismo.aittooja@gmail.com>
2024-01-26 14:08:17 +0200

    Check if username is taken

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.sql import text
@@ -34,9 +34,36 @@ def create_account():
 
 @app.route("/add_student", methods=["POST"])
 def add_student():
+    # Tämä antaa siis usernamen siinä muodossa kun se syötettiin lomakkeeseen: Tyyliin "Keijo"
+    username = request.form["username"]
+
+
+
+    #TEE TÄSTÄ OMA FUNKTIO:
+    sql = text("SELECT * FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user:
+        message = "Käyttäjätunnus on varattu"
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        value1 = "value=" + firstname
+        value2 = "value=" + lastname
+        value3 = "value=" + username
+        value4 = "value=" + password1
+        value5 = "value=" + password2
+        return render_template("create_account.html", message=message,
+                                                        value1=value1,
+                                                        value2=value2,
+                                                        value3=value3,
+                                                        value4=value4,
+                                                        value5=value5)
+    
     realname = request.form["firstname"]
     realname += " " + request.form["lastname"]
-    username = request.form["username"]
     password = request.form["password1"]
     sql = text("INSERT INTO users (realname, username, password) VALUES (:realname, " \
                ":username, :password) RETURNING id")
