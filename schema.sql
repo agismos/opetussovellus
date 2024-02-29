ea738faf328147b50544328ddefdf4346325ead7
Ismo <ismo.aittooja@gmail.com>
2024-02-29 14:20:41 +0200

    Rearrange tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,54 @@
-CREATE TABLE students (id SERIAL PRIMARY KEY, realname TEXT, username TEXT UNIQUE, password TEXT, rights TEXT DEFAULT 'student');
-CREATE TABLE teachers (id SERIAL PRIMARY KEY, realname TEXT, username TEXT UNIQUE, password TEXT, rights TEXT DEFAULT 'teacher');
-CREATE TABLE usernames (id SERIAL PRIMARY KEY, username TEXT UNIQUE, rights TEXT);
-CREATE TABLE courses (id SERIAL PRIMARY KEY, course_name TEXT UNIQUE, credits INTEGER, contents TEXT, teacher_name TEXT, teacher_username TEXT);
-CREATE TABLE enrollments (id SERIAL PRIMARY KEY, course_id INTEGER REFERENCES courses(id), course_name TEXT, student_username TEXT REFERENCES students(username), teacher_username TEXT REFERENCES teachers(username));
-CREATE TABLE questions (id SERIAL PRIMARY KEY, course_id INTEGER, course_name TEXT, question TEXT, answer TEXT, is_correct BOOLEAN);
-CREATE TABLE results (id SERIAL PRIMARY KEY, course_name TEXT REFERENCES courses(course_name), student_username TEXT REFERENCES students(username), points INTEGER);
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    realname TEXT,
+    username TEXT UNIQUE,
+    password TEXT,
+    rights TEXT DEFAULT 'student'
+);
+
+CREATE TABLE teachers (
+    id SERIAL PRIMARY KEY,
+    realname TEXT,
+    username TEXT UNIQUE,
+    password TEXT,
+    rights TEXT DEFAULT 'teacher'
+);
+
+CREATE TABLE usernames (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    rights TEXT
+);
+
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    course_name TEXT UNIQUE,
+    credits INTEGER,
+    contents TEXT,
+    teacher_name TEXT,
+    teacher_username TEXT
+);
+
+CREATE TABLE enrollments (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES courses(id),
+    course_name TEXT,
+    student_username TEXT REFERENCES students(username),
+    teacher_username TEXT REFERENCES teachers(username)
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER,
+    course_name TEXT,
+    question TEXT,
+    answer TEXT,
+    is_correct BOOLEAN
+);
+
+CREATE TABLE results (
+    id SERIAL PRIMARY KEY,
+    course_name TEXT REFERENCES courses(course_name),
+    student_username TEXT REFERENCES students(username),
+    points INTEGER
+);
